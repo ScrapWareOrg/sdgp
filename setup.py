b509fbba77606cb302c2224d11a44bb2583ed76b
ScrapWareOrg <github@twinkle.work>
2025-07-30 13:27:58 +0900

    0.92.0

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ setup(
     name='sdgp',
     version='0.92.0',
     packages=find_packages(exclude=['tests']),
-    ext_modules=[
-        gtk_dialog_module
-    ],
     entry_points={
         'console_scripts': [
             'my_command = sdgp.module:main_func',
